f5a4ef4e9bc8f53cd01aacfde52016cc1f2b0abb
unloosedcascade <matthewdan.handley@btinternet.com>
2022-03-14 17:50:37 +0000

    Backing up work done today 14/03/2022

--- src/main/java/com/sparta/SortManagerProject/Main/Main.java
+++ src/main/java/com/sparta/SortManagerProject/Main/Main.java
@@ -1,4 +1,17 @@
 package com.sparta.SortManagerProject.Main;
 
+import com.sparta.SortManagerProject.SortingAlgorithms.BinaryTreeSort;
+
+
 public class Main {
+    public static void main(String[] args) {
+        BinaryTreeSort tree = new BinaryTreeSort(5);
+            tree.addElement(11);
+            tree.addElement(12);
+            tree.addElement(13);
+            tree.addElement(14);
+            tree.addElement(15);
+            tree.addElement(16);
+        ;
+        System.out.println(tree.findElement(13));   }
 }
---
+++ src/main/java/com/sparta/SortManagerProject/SortingAlgorithms/Main.java
@@ -0,0 +1,8 @@
+package com.sparta.SortManagerProject.SortingAlgorithms;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
